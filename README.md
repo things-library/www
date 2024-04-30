a84ad8546dcb5eaa054767a1def0a307490724c7
Mark Lanning <mlanning@wittern.com>
2024-04-30 00:27:32 -0500

    updated schemas

--- README.md
+++
@@ -1,3 +0,0 @@
-# Public Static Website
-
-This repository is intended to be the future location of the public facing static website.
---
+++ js/site.js
@@ -0,0 +1,20 @@
+﻿function Page_Reload() { window.location.reload(); }
+
+function Page_Redirect(url) {
+    window.top.location.replace(url);
+}
+
+function CopyToClipboard(elementId, sender) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(elementId).html()).select();
+    document.execCommand("copy");
+    $temp.remove();
+
+    if (sender) {
+        sender.textContent = 'Copied';
+    }
+    else {
+        alert('Copied to Clipboard');
+    }
+}
